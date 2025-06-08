a060b2f5e8861e21b666108044c3dc713a1fb34a
BuiHuyTam <bhtam28@gmail.com>
2025-06-08 09:12:53 +0000

    feat: configure better-auth with prisma adapter for server side

--- backend/auth/database.ts
+++ backend/auth/database.ts
@@ -1,6 +1,5 @@
 import { SQLDatabase } from "encore.dev/storage/sqldb";
 import { PrismaClient } from "@prisma/client";
-import { url } from "inspector";
 
 const DB = new SQLDatabase(
   "auth",
--- backend/auth/encore.service.ts
+++ backend/auth/encore.service.ts
@@ -1,3 +1,21 @@
 import { Service } from "encore.dev/service";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+// If prisma file located elsewhere
+import { PrismaClient } from "./generated/prisma";
+import { betterAuth } from "better-auth";
+import prisma from "./database";
+
+export const auth = betterAuth({
+  // Initialize database
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  // Initialize email & password authentication
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+  },
+  trustedOrigins: ["http://localhost:3000"],
+})
 
 export default new Service("auth");
